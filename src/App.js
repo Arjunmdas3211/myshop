e9ca275ae8f19c984c560a3c9c27f84710481ef1
Arjun Mohandas <48945581+Arjunmdas3211@users.noreply.github.com>
2020-08-15 13:53:14 +0530

    Included react dom router and navigation in menu-item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import HomePage from '../src/pages/homepage/homepage.component';
+import { Switch , Route} from 'react-router-dom';
+
+const hatspage = () => (
+<div>
+  <h1>HatSpage</h1>
+</div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={hatspage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import { withRouter } from 'react-router-dom';
+
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl,size}) => (
+const MenuItem = ({title, imageUrl,size, history, linkUrl, match }) => (
     <div 
-    className={`${size} menu-item`}>
+    className={`${size} menu-item`} onClick = {() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image'  style={{ background: `url(${imageUrl})`}} />
                <div className='content'>
                    <h1 className='title'>{title.toUpperCase()}</h1>
@@ -12,4 +15,4 @@ const MenuItem = ({title, imageUrl,size}) => (
                </div>
            </div>
 )
-export default MenuItem;
+export default withRouter(MenuItem);

106f2d4303cdb104d5ccb9cf9ea9cc87e4618fc5
Arjunmdass321 <arjunmdas321@gmail.com>
2020-10-06 11:53:02 +0530

    Feat(API): Add stripe integration and updated icon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51HYtKcFJo0QElKHXVL5UKRL0E9YMFQhnQ6CxFLCWDKoFKGcFSZVCfJ0zNOK858XzlEa7E9dzoksV8Oeo3gshagIf00I5gKjrwk';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total price is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
